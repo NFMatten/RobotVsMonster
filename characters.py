1a580d1d3b774a7f1b2dd67cba104cf74841cf9f
Nikolaus Matten <nfmatten@gmail.com>
2022-08-25 08:15:20 -0600

    feat: initial built classes

---
+++ characters.py
@@ -0,0 +1,15 @@
+
+class Robot:
+    def __init__(self, name, health, active_weapon):
+        self.name = name
+        self.health = health
+        self.active_weapon = active_weapon
+
+
+
+
+class Monster:
+    def __init__(self, name, health, active_weapon):
+        self.name = name
+        self.health = health
+        self.active_weapon = active_weapon 
