30dcf173aa2e45d976370c4c1d92f9073fe3757f
Nikolaus Matten <nfmatten@gmail.com>
2022-08-25 20:22:00 -0600

    style: added purposes for certain code in Battlefield class. Reminder for where to pick up

--- battlefield.py
+++ battlefield.py
@@ -19,13 +19,17 @@ class Battlefield:
         c3po = Robot("C-3PO")
         rex = Dinosaur("Rex", 15)
         pause_duration = 1
+        # Continues fight until one opponent health reaches 0 
         while c3po.health > 0 and rex.health > 0:
-            time.sleep(pause_duration)
-            print(f"{c3po.name} attacked {rex.name} with a {c3po.active_weapon.name}\nRex Health: {rex.health}")
+            time.sleep(pause_duration) # Slows program to see each attack more clearly
+
+            # c3po attacks
             c3po.attack(rex)
+            print(f"{c3po.name} attacked {rex.name} with a {c3po.active_weapon.name} for {c3po.active_weapon.attack_power}\nRex Health: {rex.health}")
+            # rex attacks
             rex.attack(c3po)
-            print(f"C-3PO Health: {c3po.health}\t ")
-            print()
+            print(f"{rex.name} attacked {c3po.name} for {rex.attack_power}. C-3PO Health: {c3po.health}")
+            # if opponent loses, print winner (To be updated with "display_winner" method)
             if c3po.health <= 0:
                 c3po.health = 0
                 print("C3PO lost")
@@ -33,6 +37,7 @@ class Battlefield:
                 rex.health = 0
                 print("Rex lost")
 
+    # Purpose: Prints who won
     def display_winner(self):
         c3po = Robot("C-3PO")
         rex = Dinosaur("Rex", 15)
